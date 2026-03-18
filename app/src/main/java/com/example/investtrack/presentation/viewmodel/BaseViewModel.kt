a49fdac73da7b6a59c9820011c1e2f71a6090a2c
Akanksha199 <50025665+Akanksha199@users.noreply.github.com>
2026-03-19 00:43:52 +0530

    Base Classes Setup - ITA 6 (#3)
    * Test PR flow
    
    * Base classes setup

---
+++ app/src/main/java/com/example/investtrack/presentation/viewmodel/BaseViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.investtrack.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+open class BaseViewModel : ViewModel() {
+
+    protected fun launch(
+        block: suspend CoroutineScope.() -> Unit
+    ) {
+        viewModelScope.launch {
+            try {
+                block()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
