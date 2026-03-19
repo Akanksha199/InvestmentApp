d87c2fe8182d7b8cf8547031745ca8c0ee22b552
Akanksha Saxena <akanshasaxena1999@gmail.com>
2026-03-19 21:04:42 +0530

    theme setup

--- app/src/main/java/com/example/investtrack/MainActivity.kt
+++ app/src/main/java/com/example/investtrack/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.investtrack.presentation.navigation.AppNavGraph
-import com.example.investtrack.ui.theme.InvestTrackTheme
+import com.example.investtrack.presentation.ui.theme.InvestTrackTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
